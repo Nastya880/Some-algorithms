55de089a0d735088f7a626206f61e3ee04280079
Nastya880 <oidmakeevy@gmail.com>
2023-09-13 19:35:01 +0300

    +perimetr

---
+++ perimetr/solution.js
@@ -0,0 +1,25 @@
+const matrix = [
+  'XOOXO',
+  'XOOXO',
+  'OOOXO',
+  'XXOXO',
+  'OXOOO'
+]
+
+// учтено, что (1 + true = 2)
+function perimiter(matrix) { // O(N*N) - вложенный цикл
+  let p = 0;
+  for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+          if(matrix[i][j] === 'X') {
+              p += j === 0 || matrix[i][j - 1] === "O"
+              p += i === 0 || matrix[i - 1][j] === "O"
+              p += i === matrix.length - 1 || matrix[i + 1][j] === "O"
+              p += j === matrix[i].length - 1 || matrix[i][j + 1] === "O"
+          }
+      }
+  }
+  return p;
+}
+
+console.log(perimiter(matrix))
