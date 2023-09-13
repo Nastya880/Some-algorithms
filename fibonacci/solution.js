1146e879c3b56dc1e51a0a43be31224e1ed25211
Nastya880 <oidmakeevy@gmail.com>
2023-09-13 14:45:17 +0300

    + task-fibonacci

---
+++ fibonacci/solution.js
@@ -0,0 +1,18 @@
+const fibonacci = n => {
+  if (n <= 0) {
+        return 0;
+    }
+    if (n <= 2) {
+        return 1;
+    }
+    let prev = 1;
+    let result = 1;
+    for (let i = 0; i < n - 2; i++) {
+        let tmp = result;
+        result += prev;
+        prev = tmp;
+    }
+    return result;
+};
+
+// console.log(fibonacci(7))
